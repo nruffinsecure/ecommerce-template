ef14129b2dcc896f533e12995fc2cf675edc6575
Nick Ruffin <nruffinsecure@gmail.com>
2024-11-29 14:12:52 -0600

    massive commit setting up products prior to cart setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page
-    applyBaseStyles: false,
+    config: { path: './tailwind.config.cjs' },
+    applyBaseStyles: false
   })],
 });
