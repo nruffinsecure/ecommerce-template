5751a5e5e5e6557e00d4249bfbd1f2bccb9caed8
Nick Ruffin <nruffinsecure@gmail.com>
2024-11-29 19:07:19 -0600

    updated blog page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,7 @@ const Header: React.FC = () => {
   const navItems: NavItem[] = [
     { name: 'Home', path: '/' },
     { name: 'Products', path: '/products' },
+    { name: 'Blog', path: '/blog'},
     { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
     { name: 'Cart', path: '/cart' }
